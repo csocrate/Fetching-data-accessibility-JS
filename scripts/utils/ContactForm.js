038fea4970c8110cec920698015771b56f5de696
Devmatrix <devmatrix73@gmail.com>
2023-05-03 18:03:30 +0200

    Add submission form when inputs are valid

--- scripts/utils/ContactForm.js
+++ scripts/utils/ContactForm.js
@@ -3,8 +3,9 @@ class ContactForm {
     this._photographer  = photographer
     
     this.$contactModal  = document.querySelector('#contact_modal')
-    this.$contactButton = document.querySelector('.contact_button')
-    this.$closeModal    = document.querySelector('.close_modal')
+    this.$openModalBtn  = document.querySelector('.photograph-header .contact_button')
+    this.$closeModalBtn = document.querySelector('.close_modal')
+    this.$form          = document.querySelector('form')
     this.$body          = document.querySelector('body')
   }
 
@@ -13,23 +14,27 @@ class ContactForm {
   }
 
   displayModal() {
-    this.$contactButton.addEventListener('click', () => {
+    this.$openModalBtn.addEventListener('click', () => {
 
-      this.$contactModal.style.display = "flex"
+      this.$contactModal.style.display = 'flex'
 
-      this.$body.classList.add("visible_modal")
+      this.$body.classList.add('visible_modal')
       this.$body.querySelector('header').setAttribute('aria-hidden', 'true')
       this.$body.querySelector('main').setAttribute('aria-hidden', 'true')
       this.$contactModal.setAttribute('aria-hidden', 'false')
+
+      this.$closeModalBtn.setAttribute('aria-label', 'Fermer le formulaire de contact')
+      this.$closeModalBtn.setAttribute('title', 'Fermer le formulaire de contact')
+      this.$closeModalBtn.setAttribute('aria-expanded', 'true')
     })
   }
 
   closeModal() {
-      this.$closeModal.addEventListener('click', () => {
+      this.$closeModalBtn.addEventListener('click', () => {
 
-        this.$contactModal.style.display = "none"
+        this.$contactModal.style.display = 'none'
 
-        this.$body.classList.remove("visible_modal")
+        this.$body.classList.remove('visible_modal')
         this.$body.querySelector('header').setAttribute('aria-hidden', 'false')
         this.$body.querySelector('main').setAttribute('aria-hidden', 'false')
         this.$contactModal.setAttribute('aria-hidden', 'true')
@@ -42,29 +47,102 @@ class ContactForm {
 
   createContactForm() {
     const contactForm = `
-      <label for="firstname">
+      <label for='firstname'>
         Prénom
       </label>
-      <input type="text" name="firstname" id="firstname" autocomplete="given-name" aria-required="true" required>
-      <label for="lastname">
+      <input type='text' name='firstname' id='firstname' autocomplete='given-name' aria-required='true'>
+      <p>Le champ prénom doit être renseigné</p>
+      <label for='lastname'>
         Nom
       </label>
-      <input type="text" name="lastname" id="lastname" autocomplete="family-name">
-      <label for="email">
+      <input type='text' name='lastname' id='lastname' autocomplete='family-name' aria-required='true'>
+      <p>Le champ nom doit être renseigné</p>
+      <label for='email'>
         Email
       </label>
-      <input type="email" name="email" id="email" autocomplete="email" aria-required="true" required>
-      <label for="">
+      <input type='email' name='email' id='email' autocomplete='email' aria-required='true'>
+      <p>Le champ email doit être renseigné</p>
+      <label for=''>
         Votre message
       </label>
-      <textarea name="message" id="message" cols="30" rows="10" autocomplete="on" aria-required="true" required></textarea>`
+      <textarea name='message' id='message' cols='30' rows='10' autocomplete='on' aria-required='true'></textarea>
+      <p>Le champ message doit être renseigné</p>`
 
-    this.$contactModal.querySelector('form > div').innerHTML = contactForm
+    this.$form.querySelector('div').innerHTML = contactForm
+  }
+
+  isInputNotEmpty() {
+    const firstname = this.$form.firstname
+    const lastname  = this.$form.lastname
+    const email     = this.$form.email
+    const message   = this.$form.message
+
+    let result = true
+
+    if (!firstname.value.length) {
+      firstname.dataset.errorVisible = "true"
+      firstname.setAttribute('aria-invalid', 'true')
+      this.$form.querySelector('input[id=firstname] ~ p').style.display = "block"
+      result = false
+    } else {
+      console.log(`Prénom: ${firstname.value}`)
+      firstname.dataset.errorVisible = "false"
+      firstname.setAttribute('aria-invalid', 'false')
+      this.$form.querySelector('input[id=firstname] ~ p').style.display = "none"
+    }
+
+    if (!lastname.value.length) {
+      lastname.dataset.errorVisible = "true"
+      lastname.setAttribute('aria-invalid', 'true')
+      this.$form.querySelector('input[id=lastname] ~ p').style.display = "block"
+      result = false
+    } else {
+      console.log(`Nom: ${lastname.value}`)
+      lastname.dataset.errorVisible = "false"
+      lastname.setAttribute('aria-invalid', 'false')
+      this.$form.querySelector('input[id=lastname] ~ p').style.display = "none"
+    }
+
+    if (!email.value.length) {
+      email.dataset.errorVisible = "true"
+      email.setAttribute('aria-invalid', 'true')
+      this.$form.querySelector('input[id=email] ~ p').style.display = "block"
+      result = false
+    } else {
+      console.log(`Email: ${email.value}`)
+      email.dataset.errorVisible = "false"
+      email.setAttribute('aria-invalid', 'false')
+      this.$form.querySelector('input[id=email] ~ p').style.display = "none"
+    }
+
+    if (!message.value.length) {
+      message.dataset.errorVisible = "true"
+      message.setAttribute('aria-invalid', 'true')
+      this.$form.querySelector('textarea ~ p').style.display = "block"
+      result = false
+    } else {
+      console.log(`Message: ${message.value}`)
+      message.dataset.errorVisible = "false"
+      message.setAttribute('aria-invalid', 'false')
+      this.$form.querySelector('textarea ~ p').style.display = "none"
+    }
+
+    if (result === true) {
+      console.log("Merci pour votre message.")
+    }
+  }
+  
+  submitForm() {
+    this.$form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      this.isInputNotEmpty()
+    })
   }
 
   render() {
     this.displayModal()
     this.closeModal()
     this.createContactForm()
+    this.submitForm()
   }
 }
